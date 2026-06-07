cc6fdb1bb83b3d5cb90d94ad43aea8137bb2f68f
ffost310 <torantikplay@gmail.com>
2026-06-07 23:49:31 +0300

    Add LevelGen scene with script referencing floor modules (empty scaffold)

---
+++ Scripts/LevelGen.gd
@@ -0,0 +1,5 @@
+extends Node2D
+
+const FLOOR_EMPTY = preload("res://Scenes/Floors/Floor_Empty.tscn")
+const FLOOR_ELEVATOR = preload("res://Scenes/Floors/Floor_Elevator.tscn")
+const FLOOR_RECTANGLE = preload("res://Scenes/Floors/Floor_Rectangle.tscn")
