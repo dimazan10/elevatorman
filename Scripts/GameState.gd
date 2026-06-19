5de918334ad609aa3d27797d9c1499b81fb0f698
dimazan <dymazalugovskij@gmail.com>
2026-06-19 14:29:05 +0200

    persist master/music volume and current_floor to user://settings.cfg via ConfigFile

--- Scripts/GameState.gd
+++ Scripts/GameState.gd
@@ -1,17 +1,38 @@
 extends Node
 
+const SAVE_PATH := "user://settings.cfg"
+
 var current_floor: int = 1
 var master_volume: float = 0.0
 var music_volume: float = 0.0
 
 func _ready() -> void:
+	_load_settings()
+
+func _load_settings() -> void:
+	var cfg := ConfigFile.new()
+	var err = cfg.load(SAVE_PATH)
+	if err != OK:
+		return
+	master_volume = cfg.get_value("audio", "master_volume", 0.0)
+	music_volume = cfg.get_value("audio", "music_volume", 0.0)
 	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), master_volume)
 	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Music"), music_volume)
+	current_floor = cfg.get_value("game", "current_floor", 1)
+
+func _save_settings() -> void:
+	var cfg := ConfigFile.new()
+	cfg.set_value("audio", "master_volume", master_volume)
+	cfg.set_value("audio", "music_volume", music_volume)
+	cfg.set_value("game", "current_floor", current_floor)
+	cfg.save(SAVE_PATH)
 
 func set_master_volume(db: float) -> void:
 	master_volume = db
 	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), db)
+	_save_settings()
 
 func set_music_volume(db: float) -> void:
 	music_volume = db
 	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Music"), db)
+	_save_settings()
