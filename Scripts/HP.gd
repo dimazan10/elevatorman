0d75fdb3d97ac952bcc90c17d126bd271fc5b6b7
dimazan <dymazalugovskij@gmail.com>
2026-06-25 17:10:27 +0200

    After 10 hearts, show xN overflow label instead of more hearts

--- Scripts/HP.gd
+++ Scripts/HP.gd
@@ -1,39 +1,54 @@
 extends HBoxContainer
 
+const MAX_HEARTS = 10
+
 var pulse_tween: Tween
 var _heart_tex: Texture2D = preload("res://Assets/heart.png")
+var _overflow_label: Label
 
 func _ready() -> void:
 	await get_tree().process_frame
 	for heart in get_children():
 		heart.pivot_offset = Vector2(32, 32)
+	_overflow_label = Label.new()
+	_overflow_label.add_theme_font_size_override("font_size", 28)
+	_overflow_label.add_theme_color_override("font_color", Color.WHITE)
+	_overflow_label.visible = false
+	add_child(_overflow_label)
 	var player = get_tree().get_first_node_in_group("player")
 	if player:
 		player.health_changed.connect(update_hearts)
 
 func update_hearts(current_health: int):
-	var hearts = get_children()
-	var diff = hearts.size() - current_health
+	var hearts = []
+	for c in get_children():
+		if c is TextureRect:
+			hearts.append(c)
 
-	if diff > 0:
-		var hearts_to_remove = clampi(diff, 0, hearts.size())
-		for i in range(hearts_to_remove):
-			var last_heart = hearts[hearts.size() - 1 - i]
-			last_heart.queue_free()
-	elif diff < 0:
-		var template = hearts[0] if hearts.size() > 0 else null
-		for i in range(-diff):
-			var new_heart: TextureRect
-			if template:
-				new_heart = template.duplicate()
-			else:
-				new_heart = TextureRect.new()
-				new_heart.texture = _heart_tex
-				new_heart.custom_minimum_size = Vector2(64, 64)
-				new_heart.stretch_mode = 5
-				new_heart.texture_filter = CanvasItem.TEXTURE_FILTER_LINEAR
-			new_heart.pivot_offset = Vector2(32, 32)
-			add_child(new_heart)
+	var visible_count = mini(current_health, MAX_HEARTS)
+	var overflow = current_health - MAX_HEARTS
+
+	while hearts.size() > visible_count:
+		var last = hearts.pop_back()
+		last.queue_free()
+
+	var template = _heart_tex
+	while hearts.size() < visible_count:
+		var new_heart = TextureRect.new()
+		new_heart.texture = template
+		new_heart.custom_minimum_size = Vector2(64, 64)
+		new_heart.expand_mode = 1
+		new_heart.stretch_mode = 5
+		new_heart.texture_filter = CanvasItem.TEXTURE_FILTER_LINEAR
+		new_heart.pivot_offset = Vector2(32, 32)
+		add_child(new_heart)
+		hearts.append(new_heart)
+
+	if overflow > 0:
+		_overflow_label.text = "x" + str(overflow)
+		_overflow_label.visible = true
+	else:
+		_overflow_label.visible = false
 
 	if current_health == 1:
 		start_last_heart_effect()
