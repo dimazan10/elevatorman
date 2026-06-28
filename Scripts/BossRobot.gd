6b740035139e86d9877168100f49092dc4751c5f
ffost310 <torantikplay@gmail.com>
2026-06-28 18:42:29 +0300

    Add pause menu support to boss_robot scene

--- Scripts/BossRobot.gd
+++ Scripts/BossRobot.gd
@@ -5,6 +5,7 @@ const DashUI := preload("res://Objects/DashUI.tscn")
 const InventoryUI := preload("res://Objects/InventoryUI.tscn")
 
 func _ready() -> void:
+	add_to_group("pausable")
 	add_child(HealthUI.instantiate())
 	add_child(DashUI.instantiate())
 	add_child(InventoryUI.instantiate())
