b90c2a2307d050f30f4d2aa19e035bb6cc597537
ffost310 <torantikplay@gmail.com>
2026-07-06 20:09:06 +0300

    earthquake sound plays at start of DownFallBox animation (in _ready)

--- Scripts/Box.gd
+++ Scripts/Box.gd
@@ -4,20 +4,20 @@ const PATRON_SCENE := preload("res://Objects/Boss/Robot/Patron.tscn")
 @onready var _anim := $Sprite2D/AnimationPlayer
 
 func _ready():
+	var audio := AudioStreamPlayer2D.new()
+	audio.name = "EarthquakeAudio"
+	audio.stream = load("res://Assets/Enemies/Boss/Sprite_Robot/earthquake_sound.mp3")
+	audio.bus = &"Effects"
+	audio.global_position = global_position
+	get_parent().add_child(audio)
+	audio.play()
+	audio.finished.connect(audio.queue_free)
+
 	_anim.animation_finished.connect(_on_anim_finished)
 	_anim.play("DownFallBox")
 
 func _on_anim_finished(anim: String):
 	if anim == "DownFallBox":
-		var audio := AudioStreamPlayer2D.new()
-		audio.name = "EarthquakeAudio"
-		audio.stream = load("res://Assets/Enemies/Boss/Sprite_Robot/earthquake_sound.mp3")
-		audio.bus = &"Effects"
-		audio.global_position = global_position
-		get_parent().add_child(audio)
-		audio.play()
-		audio.finished.connect(audio.queue_free)
-
 		var patron = PATRON_SCENE.instantiate()
 		patron.global_position = global_position
 		get_parent().add_child(patron)
