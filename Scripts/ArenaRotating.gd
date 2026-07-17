f9f668c36fbc4ebe2bda389e830eaf68cdbb350a
FFost <torantikplay@gmail.com>
2026-07-17 19:52:33 +0300

    fix: elevator collision walls removed, player lowered, arrival sequence, ArenaSwitch stopped

--- Scripts/ArenaRotating.gd
+++ Scripts/ArenaRotating.gd
@@ -1,8 +1,3 @@
 extends Node2D
 
-var rotation_speed: float = 0.5
-
-func _physics_process(delta: float) -> void:
-	var pivot = $Pivot
-	if pivot:
-		pivot.rotation += delta * rotation_speed
+var rotation_speed: float = 0.0
